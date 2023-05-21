e669e2b6ee281e2de509f05117a10ae3624b838f
Bassel Mawla <basselalmawla@gmail.com>
2023-05-21 19:09:33 +0100

    Generate and statically draw the 3 reels

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # slot-machine
 
 A simple slot machine game using Pixi.js.
-Requires pixi.js in the main directory, which can be downloaded from https://pixijs.com/.
+Requires pixi.mjs in the main directory, which can be downloaded from https://pixijs.com/.
--- main.js
+++ main.js
@@ -9,7 +9,15 @@ const app = new PIXI.Application({
 await PIXI.Assets.load([
   "sprites/slot-machine1.png",
   "sprites/slot-symbol1.png",
+  "sprites/slot-symbol2.png",
+  "sprites/slot-symbol3.png",
 ]);
+
+const symbolTextures = {
+  symbol1: PIXI.Texture.from("sprites/slot-symbol1.png"),
+  symbol2: PIXI.Texture.from("sprites/slot-symbol2.png"),
+  symbol3: PIXI.Texture.from("sprites/slot-symbol3.png"),
+};
 
 document.body.appendChild(app.view);
 
@@ -27,17 +35,65 @@ slotMachine.scale.set(
 );
 
 const symbolCoords = {
-  firstX: -125,
-  secondX: 5,
-  thirdX: 135,
-  y: 40,
+  xLeft: -125,
+  xMiddle: 5,
+  xRight: 135,
+  yTop: -75,
+  yMiddle: 40,
+  yBottom: 163,
 };
 
-const symbol1 = PIXI.Sprite.from("sprites/slot-symbol1.png");
-slotMachine.addChild(symbol1);
-symbol1.anchor.set(0.5);
-symbol1.x = symbolCoords.firstX;
-symbol1.y = symbolCoords.y;
-console.log(slotMachine.x);
+// Create the three reels
+const reelsList = [];
+
+for (let i = 0; i < 3; i++) {
+  const reel = new PIXI.Container();
+  //reel.anchor.set(0.5);
+
+  // Set x coordinate of reelContainer based on i
+  switch (i) {
+    case 0:
+      reel.x = symbolCoords.xLeft;
+      break;
+    case 1:
+      reel.x = symbolCoords.xMiddle;
+      break;
+    default: // case 2
+      reel.x = symbolCoords.xRight;
+      break;
+  }
+
+  //const reel = {
+  //  container: reelContainer,
+  //  children: [],
+  //};
+
+  // 3 symbols in each reel
+  let index = 0;
+  for (const texture of Object.values(symbolTextures)) {
+    const symbol = new PIXI.Sprite(texture);
+    symbol.anchor.set(0.5);
+    reel.addChild(symbol);
+    //reel.children.push(symbol);
+    console.log(symbol.height);
+
+    // Set y coordinate of the symbol based on index
+    switch (index) {
+      case 0:
+        symbol.y = symbolCoords.yTop;
+        break;
+      case 1:
+        symbol.y = symbolCoords.yMiddle;
+        break;
+      default: // case 2
+        symbol.y = symbolCoords.yBottom;
+        break;
+    }
+
+    reelsList.push(reel);
+    slotMachine.addChild(reel);
+    index++;
+  }
+}
 
 app.stage.addChild(slotMachine);
