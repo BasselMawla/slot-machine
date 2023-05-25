ef1004e239987861b242032747891bbdbbc57acf
Bassel Mawla <basselalmawla@gmail.com>
2023-05-25 01:29:32 +0100

    Lever animation extra frame
    Added an extra frame to the lever pulling animation. It consists of the 2 lever sprites overlapped with an alpha of 0.5 each.

--- lever.js
+++ lever.js
@@ -4,29 +4,72 @@ import app from "./main.js";
 
 export default class Lever extends PIXI.Container {
   leverSprite;
+  extraFrameSprites;
   isPulled;
   static elapsedTime = 0.0;
+  static animTime = 500;
+  static extraFrameTime = 50;
 
   constructor() {
     super();
+
+    this.isPulled = false;
+
     this.leverSprite = new PIXI.Sprite(Textures.leverInitial);
     this.leverSprite.anchor.set(0.5);
     this.addChild(this.leverSprite);
-    this.isPulled = false;
+
+    // Add an in between frame for the lever pulling animation
+    // A combination of both lever textures each with 50% alpha
+    this.extraFrameSprites = [
+      new PIXI.Sprite(Textures.leverInitial),
+      new PIXI.Sprite(Textures.leverPulled),
+    ];
+    for (let i = 0; i < this.extraFrameSprites.length; i++) {
+      this.extraFrameSprites[i].anchor.set(0.5);
+      this.extraFrameSprites[i].alpha = 0.5;
+      this.extraFrameSprites[i].visible = false;
+      this.addChild(this.extraFrameSprites[i]);
+    }
   }
 
-  // Animate the lever being pulled and returning to its initial position
-  // Uses TweenJS for the animation
+  // Start pulling the lever and show extra frame
   pullLever() {
+    this.showExtraFrame();
     this.leverSprite.texture = Textures.leverPulled;
     app.ticker.add(Lever.animateLever, this);
   }
 
+  showExtraFrame() {
+    for (let i = 0; i < this.extraFrameSprites.length; i++) {
+      this.extraFrameSprites[i].visible = true;
+    }
+    this.leverSprite.visible = false;
+  }
+
+  hideExtraFrame() {
+    for (let i = 0; i < this.extraFrameSprites.length; i++) {
+      this.extraFrameSprites[i].visible = false;
+    }
+    this.leverSprite.visible = true;
+  }
+
+  // Animate the lever being pulled and returning to its initial position
   static animateLever(delta) {
-    if (Lever.elapsedTime > 500) {
-      this.leverSprite.texture = Textures.leverInitial;
+    // Lever is returned, hide extra frame and stop animating the lever.
+    if (Lever.elapsedTime > Lever.animTime + Lever.extraFrameTime) {
+      this.hideExtraFrame();
       app.ticker.remove(Lever.animateLever, this);
-      Lever.elapsedTime = 0.0;
+      Lever.elapsedTime = 0;
+    }
+    // Lever is returning. Show extra frame.
+    else if (Lever.elapsedTime > Lever.animTime) {
+      this.showExtraFrame();
+      this.leverSprite.texture = Textures.leverInitial;
+    }
+    // Lever is now pulled. Hide extra frame.
+    else if (Lever.elapsedTime > Lever.extraFrameTime) {
+      this.hideExtraFrame();
     }
 
     Lever.elapsedTime += app.ticker.elapsedMS;
